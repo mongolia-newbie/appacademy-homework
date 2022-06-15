ad21380b120794a5b7c9d65e2fdecc031002012c
ningwang <ningwangiit@gmail.com>
2022-06-14 17:31:01 -0700

    Added some important algorithms for interview later

---
+++ Key Algorithms.rb
@@ -0,0 +1,84 @@
+# 气泡排序的算法
+def bb(arr)
+    status = false
+
+    while !status
+        status = true
+        (0...arr.length-1).each do |i|
+            if arr[i] > arr[i+1]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                status = false
+            end
+        end
+    end
+
+    p arr
+end
+
+arr = [5,4,3,2,1]
+bb(arr)
+
+# 找质数的一种算法，有更简单的
+def is_prime?(num)
+  return false if num < 2
+
+  (2...num).each do |i|
+    return false if num % i == 0
+  end
+
+  true
+end
+
+#Merge Sort 算法，面试大概率考
+class Array
+  def merge_sort
+    return self if count < 2
+
+    middle = count / 2
+
+    left, right = self.take(middle), self.drop(middle)
+    sorted_left, sorted_right = left.merge_sort, right.merge_sort
+
+    merge(sorted_left, sorted_right)
+  end
+
+  def merge(left, right)
+    merged_array = []
+    until left.empty? || right.empty?
+      merged_array << ((left.first < right.first) ? left.shift : right.shift)
+    end
+
+    merged_array + left + right
+  end
+
+end
+
+
+#bSearch Solution 算法，面试大概率考
+def bsearch(nums, target)
+  return nil if nums.empty?
+
+  probe_index = nums.length / 2
+  case target <=> nums[probe_index]
+  when -1
+    bsearch(nums.take(probe_index), target)
+  when 0
+    probe_index
+  when 1
+    sub_answer = bsearch(nums.drop(probe_index + 1), target)
+    sub_answer.nil? ? nil : (probe_index + 1) + sub_answer
+  end
+  
+end
+
+# LeetCode 第一题 在不重复的integer array里寻找两个数字使之和等于目标数字，返回index
+def two_sum(nums, target)
+  hashmap = Hash.new
+  
+  nums.each_with_index do |num, i|
+      lookfor = target - num
+      return [i, hashmap[lookfor]] if hashmap.has_key?(lookfor)
+      hashmap[num] = i
+  end
+end
+
